9c8033a8726dbb330d8892b1261c8b7593516f36
lari <alarissadiniz@outlook.com>
2024-02-11 22:52:27 -0300

    feat: end of first class

--- src/app.tsx
+++ src/app.tsx
@@ -16,16 +16,7 @@ export function App() {
       <div className="h-px bg-slate-700" />
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            laboriosam hic, voluptatibus minima doloremque vero libero fugit in.
-            Expedita unde sunt magnam delectus nulla nam necessitatibus neque
-            quisquam porro ab!
-          </p>
-          <div className="absolute bottom-0 left-0 right-0 h-1/5 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </div>
+        <NoteCard />
         <NoteCard />
       </div>
     </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400 outline-none relative">
       <span className="text-sm font-medium text-slate-300">há 4 dias</span>
       <p className="text-sm leading-6 text-slate-400">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
@@ -9,6 +9,6 @@ export function NoteCard() {
         saepe voluptas quae.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/5 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </div>
+    </button>
   );
 }
